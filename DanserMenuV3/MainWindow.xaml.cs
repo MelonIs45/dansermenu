b749256c8595c108ef7c40ac3156b51735b5609a
Melons <ryanpietras45@gmail.com>
2021-05-01 19:48:13 +0100

    rewrite merge

---
+++ DanserMenuV3/MainWindow.xaml.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.Data.Sqlite;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace DanserMenuV3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var settingsJson = JObject.Parse(File.ReadAllText($@"{Directory.GetCurrentDirectory()}\settings.json"));
+            LabExt.Content = $".{settingsJson["Recording"]["Container"]}";
+
+            if (File.Exists($@"{Directory.GetCurrentDirectory()}\menu.log"))
+            {
+                File.Create("menu.log");
+            }
+        }
+
+        private void TebSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (TebSearch.Text.Length > 3)
+                {
+                    CobMaps.Items.Clear();
+                    using var connection =
+                        new SqliteConnection($"Data Source={Directory.GetCurrentDirectory()}\\danser.db");
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText =
+                        $@"
+                    SELECT *
+                    FROM beatmaps
+                    WHERE dir LIKE '%{TebSearch.Text}%'
+                    OR tags LIKE '%{TebSearch.Text}%'
+                    AND mode = 0
+                ";
+
+                    var sqlTask = Task<SqliteDataReader>.Factory.StartNew(() => { return command.ExecuteReader(); });
+                    var res = sqlTask.Result;
+
+                    while (res.Read())
+                    {
+                        var name = $"{res.GetString(3)} [{res.GetString(8)}]";
+
+                        CobMaps.Items.Add(name);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                using (var logFile = new StreamWriter("menu.log"))
+                {
+                    logFile.WriteLine($"Error: {ex}");
+                }
+            }
+            
+        }
+
+        private void BuRun_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var mapName = "";
+                var diffName = "";
+                var utils = new Utils();
+
+                if (CobMaps.Items.Count != 0)
+                {
+                    mapName = CobMaps.SelectedItem.ToString().Split('[')[0].Replace("'", "''").Trim();
+                    diffName = CobMaps.SelectedItem.ToString().Split('[')[1].Split(']')[0].Replace("'", "''");
+                }
+
+                using var connection = new SqliteConnection($"Data Source={Directory.GetCurrentDirectory()}\\danser.db");
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText =
+                    $@"
+                    SELECT *
+                    FROM beatmaps
+                    WHERE title = '{mapName}'
+                    AND version = '{diffName}'
+                ";
+
+                var sqlTask = Task<SqliteDataReader>.Factory.StartNew(() => { return command.ExecuteReader(); });
+                var res = sqlTask.Result;
+
+                while (res.Read())
+                {
+                    var md5 = res.GetString(21);
+
+                    var process = new Process();
+                    var startInfo = new ProcessStartInfo
+                    {
+                        FileName = "danser.exe",
+                        Arguments = $"{utils.FormatCommands(this, md5)}"
+                    };
+                    process.StartInfo = startInfo;
+
+                    try
+                    {
+                        process.Start();
+                    }
+                    catch (Win32Exception)
+                    {
+                        System.Windows.MessageBox.Show("danser.exe not found in the same directory as the program!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                using (var logFile = new StreamWriter("menu.log"))
+                {
+                    logFile.WriteLine($"Error: {ex}");
+                }
+            }
+        }
+
+        private void CobMode_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                var utils = new Utils();
+                var selectedItem = (ComboBoxItem)CobMode.SelectedItem;
+                switch (selectedItem.Content.ToString())
+                {
+                    case "Knockout":
+
+                        break;
+                    case "Play":
+
+                        break;
+                    case "Replay":
+                        var replayFileDialog = new Microsoft.Win32.OpenFileDialog
+                        {
+                            Filter = "osr files (*.osr)|*.osr",
+                            Title = "Open osr file"
+                        };
+                        var result = replayFileDialog.ShowDialog();
+
+                        if (result == true)
+                        {
+                            var filename = replayFileDialog.FileName;
+                            TebCurReplay.Text = filename;
+
+                            TebCurReplay.Height = 50;
+
+                            if (utils.MeasureString(TebCurReplay).Width < TebCurReplay.ActualWidth)
+                                TebCurReplay.Height = 36;
+                        }
+
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                using (var logFile = new StreamWriter("menu.log"))
+                {
+                    logFile.WriteLine($"Error: {ex}");
+                }
+            }
+
+        }
+
+        private void TebCurReplay_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var utils = new Utils();
+            TebCurReplay.Height = 50;
+            if (utils.MeasureString(TebCurReplay).Width < TebCurReplay.ActualWidth) TebCurReplay.Height = 36;
+        }
+
+        private void TebSkinName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var utils = new Utils();
+            TebSkinName.Height = 50;
+            if (utils.MeasureString(TebSkinName).Width < TebSkinName.ActualWidth) TebSkinName.Height = 36;
+        }
+
+        private void CebRecord_Checked(object sender, RoutedEventArgs e)
+        {
+            if (CebRecord.IsChecked == true)
+            {
+                TebOutName.IsEnabled = true;
+            }
+        }
+
+        private void CebRecord_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (CebRecord.IsChecked == false)
+            {
+                TebOutName.IsEnabled = false;
+            }
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            var regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+        
+        private void DecimalValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            var regex = new Regex("^[.][0-9]+$|^[0-9]*[.]{0,1}[0-9]*$");
+            e.Handled = !regex.IsMatch(e.Text);
+        }
+
+        private void BtnSkinBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var utils = new Utils();
+                var settingsJson = JObject.Parse(File.ReadAllText($@"{Directory.GetCurrentDirectory()}\settings.json"));
+
+                var skinFolderDialog = new FolderBrowserDialog()
+                {
+                    SelectedPath = settingsJson["General"]["OsuSkinsDir"].ToString(),
+                };
+                var result = skinFolderDialog.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    TebSkinName.Text = skinFolderDialog.SelectedPath.Split(new char[] { '\\' }).Last();
+
+                    TebSkinName.Height = 50;
+
+                    if (utils.MeasureString(TebSkinName).Width < TebSkinName.ActualWidth)
+                        TebSkinName.Height = 36;
+                }
+            }
+            catch (Exception ex)
+            {
+                using (var logFile = new StreamWriter("menu.log"))
+                {
+                    logFile.WriteLine($"Error: {ex}");
+                }
+            }
+        }
+    }
+}
---
+++ DanserMenuV3/Utils.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Size = System.Drawing.Size;
+
+namespace DanserMenuV3
+{
+    class Utils
+    {
+        public Size MeasureString(TextBox textBox)
+        {
+            var formattedText = new FormattedText(
+                textBox.Text + "......", // Make string longer to give it some leeway, 6 periods seems give it a really clean transition
+                CultureInfo.CurrentCulture,
+                FlowDirection.LeftToRight,
+                new Typeface(textBox.FontFamily, textBox.FontStyle, textBox.FontWeight, textBox.FontStretch),
+                textBox.FontSize,
+                Brushes.Black,
+                new NumberSubstitution(),
+                1);
+
+            return new Size((int)formattedText.Width, (int)formattedText.Height);
+        }
+
+        public string FormatCommands(MainWindow mainWindow, string md5)
+        {
+            var command = "";
+
+            if (md5.Length > 0)
+            {
+                command += $" -md5={md5}";
+            }
+
+            if (mainWindow.CobMode.Text.ToLower() == "replay")
+            {
+                command += $" -replay=\"{mainWindow.TebCurReplay.Text}\"";
+            }
+            else
+            {
+                if (mainWindow.CobMode.Text.ToLower() != "none")
+                command += $" -{mainWindow.CobMode.Text.ToLower()}";
+            }
+
+            command += FormatNumberArgs(mainWindow);
+
+            if (mainWindow.CebRecord.IsChecked == true && mainWindow.CobMode.Text.ToLower() != "play")
+            {
+                if (mainWindow.TebOutName.Text != "")
+                {
+                    command += $" -out=\"{mainWindow.TebOutName.Text}\"";
+                }
+                else
+                {
+                    command += $" -record";
+                }
+            }
+
+            if (mainWindow.TebMods.Text.Length > 0)
+            {
+                command += $" -mods=\"{mainWindow.TebMods.Text}\"";
+            }
+
+            if (mainWindow.ChkSkip.IsChecked == true)
+            {
+                command += $" -skip";
+            }
+
+            if (mainWindow.TebSkinName.Text != "")
+            {
+                command += $" -skin=\"{mainWindow.TebSkinName.Text}\"";
+            }
+
+            if (mainWindow.ChkDebug.IsChecked == true)
+            {
+                command += $" -debug";
+            }
+
+            using (var logFile = new StreamWriter("menu.log"))
+            {
+                logFile.WriteLine($"Command ran using: {command}");
+            }
+            return command;
+        }
+
+        public string FormatNumberArgs(MainWindow mainWindow)
+        {
+            try
+            {
+                var numberArgs = "";
+
+                if (Convert.ToInt32(mainWindow.CursorsTextBox.Text) != 1)
+                {
+                    numberArgs += $" -cursors={Convert.ToInt32(mainWindow.CursorsTextBox.Text)}";
+                }
+                if (Convert.ToInt32(mainWindow.TagCursorsTextBox.Text) != 1)
+                {
+                    numberArgs += $" -tag={Convert.ToInt32(mainWindow.TagCursorsTextBox.Text)}";
+                }
+
+                if (Convert.ToDouble(mainWindow.SpeedTextBox.Text) != 1)
+                {
+                    if (!(mainWindow.TebMods.Text.Contains("HT") || mainWindow.TebMods.Text.Contains("DT") || mainWindow.TebMods.Text.Contains("NC")))
+                    {
+                        numberArgs += $" -speed={Convert.ToDouble(mainWindow.SpeedTextBox.Text)}";
+                    }
+                }
+
+                if (Convert.ToDouble(mainWindow.PitchTextBox.Text) != 1)
+                {
+                    if (!(mainWindow.TebMods.Text.Contains("HT") || mainWindow.TebMods.Text.Contains("DT") || mainWindow.TebMods.Text.Contains("NC")))
+                    {
+                        numberArgs += $" -pitch={Convert.ToDouble(mainWindow.PitchTextBox.Text)}";
+                    }
+                }
+
+                return numberArgs;
+            }
+            
+            catch (Exception ex)
+            {
+                using (var logFile = new StreamWriter("menu.log"))
+                {
+                    logFile.WriteLine($"Error: {ex}");
+                }
+
+                return "";
+            }
+        }
+    }
+}
