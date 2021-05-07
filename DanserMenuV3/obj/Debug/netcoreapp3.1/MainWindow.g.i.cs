1c3557109ef5f945f10979da5c8c9204642a29ad
Melons <ryanpietras45@gmail.com>
2021-05-07 16:40:56 +0100

    idk

--- DanserMenuV3/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ DanserMenuV3/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -381,7 +381,7 @@ namespace DanserMenuV3 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DANSER Menu;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DANSER Menu;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
